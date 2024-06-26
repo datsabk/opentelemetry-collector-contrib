6c8cea7c2c255f3da7f76b1419c3bdbe0a3d3030
Abhishek Kothari <abhishek@abhishekkothari.in>
2024-06-26 15:15:17 +0530

    Refactor: - Common functions for get filter query & time data

--- receiver/googlecloudmonitoringreceiver/receiver.go
+++ receiver/googlecloudmonitoringreceiver/receiver.go
@@ -7,12 +7,12 @@ import (
 	"sync"
 	"time"
 
-	"go.opentelemetry.io/collector/component"
-	"go.opentelemetry.io/collector/pdata/pmetric"
 	"go.uber.org/zap"
-
 	"google.golang.org/api/option"
 	"google.golang.org/protobuf/types/known/timestamppb"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/pdata/pmetric"
 
 	"cloud.google.com/go/monitoring/apiv3/v2/monitoringpb"
 
@@ -64,8 +64,8 @@ func (m *monitoringReceiver) initialize(ctx context.Context) error {
 		client, err := monitoring.NewMetricClient(ctx, option.WithCredentialsFile(servicePath))
 		if err != nil {
 			log.Fatalf("Failed to create client: %v", err)
+			return
 		}
-		defer client.Close()
 
 		m.client = client
 	})
@@ -80,41 +80,31 @@ func (m *monitoringReceiver) initialize(ctx context.Context) error {
 	return nil
 }
 
-func calculateStartEndTime(interval, delay time.Duration) (time.Time, time.Time) {
-	// Get the current time
-	now := time.Now()
-
-	// Calculate the start time (current time + delay)
-	startTime := now.Add(delay)
-
-	// Calculate the end time (start time + interval)
-	endTime := startTime.Add(interval)
-
-	return startTime, endTime
-}
-
+// collectMetricsDataFromEndpoint collects metrics data from the specified endpoint based on the configuration.
 func (m *monitoringReceiver) collectMetricsDataFromEndpoint(ctx context.Context) (*monitoringpb.TimeSeries, error) {
 	var calStartTime time.Time
 	var calEndTime time.Time
-
 	var filterQuery string
+
+	// Iterate over each service in the configuration to calculate start/end times and construct the filter query.
 	for _, service := range m.config.Services {
 		// Define the interval and delay times
 		interval := service.Interval
 		delay := service.Delay
 
+		// Calculate the start and end times
 		calStartTime, calEndTime = calculateStartEndTime(interval, delay)
 
-		if service.ServiceName == "compute" {
-			if service.MetricName != "" {
-				filterQuery = service.MetricName
-			} else {
-				filterQuery = `metric.type = "compute.googleapis.com/instance/cpu/usage_time"`
-			}
+		// Get the filter query for the service
+		filterQuery = getFilterQuery(service)
+
+		// Log an error if the filter query is empty
+		if filterQuery == "" {
+			m.logger.Error("Internal Server Error")
 		}
 	}
 
-	// Define the request
+	// Define the request to list time series data
 	req := &monitoringpb.ListTimeSeriesRequest{
 		Name:   "projects/" + m.config.ProjectID,
 		Filter: filterQuery,
@@ -125,13 +115,19 @@ func (m *monitoringReceiver) collectMetricsDataFromEndpoint(ctx context.Context)
 		View: monitoringpb.ListTimeSeriesRequest_FULL,
 	}
 
+	// Create an iterator for the time series data
 	it := m.client.ListTimeSeries(ctx, req)
-	fmt.Println("Time series data:")
+	m.logger.Info("Time series data:")
 
 	var resp *monitoringpb.TimeSeries
+
+	// Iterate over the time series data
 	for {
 		resp, err := it.Next()
-		fmt.Println("\n resp", resp)
+		respData := fmt.Sprintf("\n\nresp => %s", resp)
+		m.logger.Info(respData)
+
+		// Handle errors and break conditions for the iterator
 		if err != nil {
 			if err.Error() == "iterator: Done" {
 				break
@@ -143,6 +139,44 @@ func (m *monitoringReceiver) collectMetricsDataFromEndpoint(ctx context.Context)
 			return nil, fmt.Errorf("failed to retrieve time series data: %v", err)
 		}
 	}
+
 	return resp, nil
 }
 
+// calculateStartEndTime calculates the start and end times based on the current time, interval, and delay.
+func calculateStartEndTime(interval, delay time.Duration) (time.Time, time.Time) {
+	// Get the current time
+	now := time.Now()
+
+	// Calculate the start time (current time - delay)
+	startTime := now.Add(-delay)
+
+	// Calculate the end time (start time + interval)
+	endTime := startTime.Add(interval)
+
+	return startTime, endTime
+}
+
+// getFilterQuery constructs a filter query string based on the provided service.
+func getFilterQuery(service Service) string {
+	var filterQuery string
+	const baseQuery = `metric.type =`
+	const defaultComputeMetric = "compute.googleapis.com/instance/cpu/usage_time"
+
+	switch service.ServiceName {
+	case "compute":
+		if service.MetricName != "" {
+			// If a specific metric name is provided, use it in the filter query
+			filterQuery = fmt.Sprintf(`%s "%s"`, baseQuery, service.MetricName)
+			return filterQuery
+		} else {
+			// If no specific metric name is provided, use the default compute metric
+			filterQuery = fmt.Sprintf(`%s "%s"`, baseQuery, defaultComputeMetric)
+			return filterQuery
+		}
+		// Add other service cases here
+	default:
+		// Return an empty string if the service is not recognized
+		return ""
+	}
+}
